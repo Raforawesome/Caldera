84de12aad9c0babcea63c690fc36ffd51cca5630
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-12-12 01:47:37 -0500

    Create `TokenStream::push` method
    Using checked unsafe code

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -14,16 +14,30 @@ impl<'a> TokenNode<'a> {
 }
 
 pub struct TokenStream<'a> {
-    first: Rc<Option<TokenNode<'a>>>,
-    last: Rc<Option<TokenNode<'a>>>,
+    first: Option<Rc<TokenNode<'a>>>,
+    last: Option<Rc<TokenNode<'a>>>,
 }
 
 impl<'a> TokenStream<'a> {
     pub fn new() -> Self {
         Self {
-            first: Rc::new(None),
-            last: Rc::new(None),
+            first: None,
+            last: None,
         }
+    }
+
+    pub fn push(&'a mut self, token: Token<'a>) {
+        let new: Rc<TokenNode> = Rc::new(TokenNode::new(token));
+        if self.last.is_none() {
+            self.first = Some(Rc::clone(&new));
+        } else {
+            let mut last: Rc<TokenNode<'a>> = self.last.clone().unwrap();
+            // we can guarantee that we're not reading tokens while writing them,
+            // as tokenization is fully completed before the `TokenStream` is provided
+            // to the caller.
+            unsafe { Rc::get_mut_unchecked(&mut last).next = Some(Rc::clone(&new)) }
+        }
+        self.last = Some(new);
     }
 }
 
